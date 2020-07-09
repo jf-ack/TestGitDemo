24d3ebeac91e8e536ecf4055fac8f0956e219c3c
jf <2451237732@qq.com>
2020-07-09 18:16:49 +0800

    测试分支提交

--- SpringbootServlet/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ SpringbootServlet/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -3,12 +3,20 @@ package com.example.demo.controller;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
     @ApiOperation(value = "hello",notes = "notes")
     @RequestMapping("/hello")
     public String hello() throws Exception{
+        return "HelloWorld";
+    }
+
+    @ApiOperation(value = "hello",notes = "notes")
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello2() throws Exception{
         return "HelloWorld";
     }
 }
